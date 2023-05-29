dd622c96ee7d02cf869a466cb67e1af0e655486b
Franco Calcina <fcalcinaf@unsa.edu.pe>
2023-05-28 21:37:36 -0500

    problema 1 de  lab2 completo

---
+++ Lab02/Ejercicio1.java
@@ -0,0 +1,26 @@
+package Lab02;
+
+public class Ejercicio1 {
+    public static void main(String []args){
+
+        int n[]={1,5,6,9,4,8};
+
+
+    }
+    public static int [] invertirArray (int[]array){
+        int nuevaM[]=new int [array.length];
+        int aux=0;
+        for(int i=array.length-1;i>=0;i--){
+            nuevaM[aux]=array[i];
+            aux++;
+        }
+        return nuevaM;
+
+    }
+    public static void Elementos(int []array){
+        for (int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+    
+}
